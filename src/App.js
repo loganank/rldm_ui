c8d24d8ab9fcfcc1d447e46cf8d84a94b17d6c2c
Logan <ldankarberg@gmail.com>
2023-11-04 14:16:01 -0600

    now looks pretty good

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
     const [messages, setMessages] = useState([]);
     const [inputMessage, setInputMessage] = useState('');
+    const inputTextAreaRef = useRef(null);
+    const maxInputRows= 8;
 
     const handleMessageSend = async () => {
         if (inputMessage.trim() === '') return;
@@ -17,11 +19,25 @@ function App() {
         setInputMessage(''); // Clear the input field
     };
 
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault(); // Prevent the default behavior of submitting the form
+            handleMessageSend(); // Call the submit function
+        }
+    };
+
     const sendMessage = async (message) => {
         // Simulate API response after a short delay
         await new Promise((resolve) => setTimeout(resolve, 10));
         return `API Response to: "${message}"`;
     };
+
+    useEffect(() => {
+        if (inputTextAreaRef.current) {
+            const newLineCount = (inputMessage.match(/\n/g) || []).length + 1; // Count the new lines
+            inputTextAreaRef.current.rows = Math.min(newLineCount, maxInputRows);
+        }
+    }, [inputMessage]);
 
     return (
         <div id="app-container" className="container-fluid text-white min-vh-100 d-flex flex-column">
@@ -29,9 +45,9 @@ function App() {
                     {messages.map((message, index) => (
                         <div
                             key={index}
-                            className={`message-container mx-auto ${
-                                message.sender === 'user' ? 'text-right' : 'text-left'
-                            }`} style={{ maxWidth: '60vw' }}
+                            className={`message-container mx-auto rounded ${
+                                message.sender === 'user' ? 'text-user' : 'text-chatbot'
+                            }`} style={{ maxWidth: '50vw' }}
                         >
                             <div
                                 className={`message p-2 ${
@@ -43,15 +59,17 @@ function App() {
                         </div>
                     ))}
                 </div>
-                <div id="chat-input" className="input-group m-3 w-50 mx-auto">
-                    <input
-                        type="text"
+                <div id="chat-input" className="input-group m-3 w-50 mx-auto bg-transparent">
+                    <textarea
+                        ref={inputTextAreaRef}
+                        rows="1" // Allow for multiple lines
                         value={inputMessage}
                         onChange={(e) => setInputMessage(e.target.value)}
+                        onKeyDown={handleKeyPress}
                         className="form-control user-input"
                         placeholder="Type a message..."
                     />
-                    <button onClick={handleMessageSend} className="btn btn-primary">
+                    <button id="send-btn" onClick={handleMessageSend} className="btn">
                         Send
                     </button>
                 </div>
