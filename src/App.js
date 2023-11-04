6c7a1b5e66318a061e1d53987b9e00a658789a50
Logan <ldankarberg@gmail.com>
2023-11-03 18:57:11 -0600

    ugly but usable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messages, setMessages] = useState([]);
+    const [inputMessage, setInputMessage] = useState('');
+
+    const handleMessageSend = async () => {
+        if (inputMessage.trim() === '') return;
+
+        const userMessage = { text: inputMessage, sender: 'user' };
+        const response = await sendMessage(inputMessage);
+        const modelMessage = { text: response, sender: 'api' };
+
+        // Add both the user and API messages to the messages array
+        setMessages([...messages, userMessage, modelMessage]);
+        setInputMessage(''); // Clear the input field
+    };
+
+    const sendMessage = async (message) => {
+        // Simulate API response after a short delay
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        return `API Response to: "${message}"`;
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div id='message-container'>
+                    {messages.map((message, index) => (
+                        <div
+                            key={index}
+                            className={`message-container ${message.sender}`}
+                        >
+                            <div className="message">{message.text}</div>
+                        </div>
+                    ))}
+                </div>
+                <div id="chat-input">
+                    <input
+                        type="text"
+                        value={inputMessage}
+                        onChange={(e) => setInputMessage(e.target.value)}
+                        placeholder="Type a message..."
+                    />
+                    <button onClick={handleMessageSend}>Send</button>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
