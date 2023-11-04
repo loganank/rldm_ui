c4f1ab22f1adc45511cb4e97eb8159441ed97079
Logan <ldankarberg@gmail.com>
2023-11-03 20:05:17 -0600

    looking better

--- src/App.js
+++ src/App.js
@@ -19,33 +19,42 @@ function App() {
 
     const sendMessage = async (message) => {
         // Simulate API response after a short delay
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+        await new Promise((resolve) => setTimeout(resolve, 10));
         return `API Response to: "${message}"`;
     };
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <div id='message-container'>
+        <div id="app-container" className="container-fluid text-white min-vh-100 d-flex flex-column">
+                <div id="message-container" className="flex-grow-1 overflow-auto p-5" style={{ maxHeight: '87vh' }}>
                     {messages.map((message, index) => (
                         <div
                             key={index}
-                            className={`message-container ${message.sender}`}
+                            className={`message-container mx-auto ${
+                                message.sender === 'user' ? 'text-right' : 'text-left'
+                            }`} style={{ maxWidth: '60vw' }}
                         >
-                            <div className="message">{message.text}</div>
+                            <div
+                                className={`message p-2 ${
+                                    message.sender === 'user' ? 'text-white' : 'text-white'
+                                }`}
+                            >
+                                {message.text}
+                            </div>
                         </div>
                     ))}
                 </div>
-                <div id="chat-input">
+                <div id="chat-input" className="input-group m-3 w-50 mx-auto">
                     <input
                         type="text"
                         value={inputMessage}
                         onChange={(e) => setInputMessage(e.target.value)}
+                        className="form-control user-input"
                         placeholder="Type a message..."
                     />
-                    <button onClick={handleMessageSend}>Send</button>
+                    <button onClick={handleMessageSend} className="btn btn-primary">
+                        Send
+                    </button>
                 </div>
-            </header>
         </div>
     );
 }
